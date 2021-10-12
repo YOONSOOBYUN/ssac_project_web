02fbcfd3587d609ac637c58ed365dd80cb7f1a2f
YOONSOOBYUN <89058100+YOONSOOBYUN@users.noreply.github.com>
2021-10-12 16:33:50 +0900

    Add files via upload

--- src/containers/common/NavbarContainer.js
+++ src/containers/common/NavbarContainer.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { useState } from "react";
 import { useContext } from "react";
 import NavbarComponent from "../../components/common/NavbarComponent";
 import AuthContext from "../../context/AuthContext";
+import { useHistory } from "react-router-dom";
 
 function NavbarContainer() {
-  const { authInfo } = useContext(AuthContext);
+  const history = useHistory();
+
+  const { authInfo, setAuthInfo } = useContext(AuthContext);
+  const [visible, setVisible] = useState(false);
+
+  const onClickProfileImg = () => {
+    setVisible(!visible);
+  };
+
+  const onClickSignOut = () => {
+    localStorage.removeItem("accessToken");
+    setAuthInfo({ isLoggedIn: false });
+    history.push("/");
+  };
   console.log(authInfo);
-  return <NavbarComponent authInfo={authInfo} />;
+  return (
+    <NavbarComponent
+      onClickProfileImg={onClickProfileImg}
+      visible={visible}
+      authInfo={authInfo}
+      onClickSignOut={onClickSignOut}
+    />
+  );
 }
 
 export default NavbarContainer;
--- src/libs/api/_client.js
+++ src/libs/api/_client.js
@@ -5,14 +5,12 @@ const client = axios.create();
 
 client.defaults.baseURL = "http://localhost:3000/";
 
-// 응답에 대한 요청을 가로채서 해당하는 부분으로 이동
 client.interceptors.response.use(
   (response) => {
     return response;
   },
   (error) => {
     const { status, data } = error.response;
-    console.log(status);
 
     if (status === 401) {
       history.push("/addprofile");
